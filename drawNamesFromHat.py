e7ff3ce37c1f3edc9d4a8f8a92948ed9766dbdbb
John Kua <jkua@velodyne.com>
2023-02-08 09:59:23 -0800

    Initial commit

---
+++ drawNamesFromHat.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import random
+
+if __name__=='__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('nameFile')
+	args = parser.parse_args()
+
+	print('\n*** Loading name file...')
+	nameFile = open(args.nameFile, 'rt')
+	lines = nameFile.readlines()
+
+	names = []
+	for line in lines:
+		name = line.strip()
+		if len(name):
+			names.append(name)
+
+	print(f'*** {len(names)} names loaded:')
+	for name in names:
+		print(f'    {name}')
+
+	print(f'\n*** Seeding random number generator...')
+	random.seed()
+
+	print(f'*** Shuffling names...')
+	random.shuffle(names)
+
+	print(f'\n*** Ready to draw! Press Enter to continue:')
+	for i, name in enumerate(names, 1):
+		input()
+		print(f'    {i}) {name}')
+
+	print(f'\n*** Done.')
+	
